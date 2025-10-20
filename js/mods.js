4bc40b94b9cd3e4a18385132f19d53acc30c2673
Drexira <daezanetwork@gmail.com>
2025-10-20 02:52:36 +0200

    Update mods.js

--- js/mods.js
+++ js/mods.js
@@ -1,5 +1,5 @@
 ﻿const API_BASE = 'https://forge.sp-tarkov.com/api/v0'
-const API_KEY = 'Z3Eh01aWyCHtvfHtZ8qYUDL6NmmHBRc0Kw9G0wAP9af00640'
+const API_KEY = 'BbMpvEF1haCKrNgUphMNeUjLMyJCwPx9XxSMBx24fa47fc7f'
 const OWN_MOD_IDS = [2330,2304,2335,2336]
 const HELPED_MOD_IDS = [2308]
 const FIELDS = 'id,name,slug,teaser,thumbnail,detail_url,downloads,featured,updated_at'
